cfe8864760d69acebc8d28b7e5511e51030d4829
Jeeva Kalaiselvam <jeevak001@outlook.com>
2022-08-02 14:45:00 +0530

    Image added

--- components/atoms/PhaseTitleHard.js
+++ components/atoms/PhaseTitleHard.js
@@ -47,8 +47,6 @@ export default function PhaseTitleNormal(props) {
     { type: TYPE_EVERY, title: "Today" },
   ];
 
-  const [currentActive, setCurrentActive] = useState;
-
   return (
     <Container
       onClick={() => {
